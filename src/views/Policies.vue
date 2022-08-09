784a1b1bcc4ffef82dc5912bc35122cfe8627b70
Cuong Nguyen <ywm.cuongnp@gmail.com>
2022-08-09 18:09:45 +0700

    upload policy page

--- src/views/Policies.vue
+++ src/views/Policies.vue
@@ -1,41 +1,120 @@
 <template>
   <div class="my-[12vh] flex flex-col justify-center items-center">
     <StarHero :heroContentType="heroContentType"></StarHero>
-    <div class="px-[20%] flex flex-col justify-center">
-      <div class="flex flex-col">
-        <span class="text-3xl">1. 사고 및 책임 보상</span>
-        <img src="../assets/policy/img-meritz.jpg" alt="" />
-        <div class="">
-          <div class="table w-full">
-            <div class="table-header-group">
-              <div class="table-row">
-                <div class="table-cell text-center">항목</div>
-                <div class="table-cell text-center">보장</div>
-                <div class="table-cell text-center">비고</div>
-              </div>
-            </div>
-            <div class="table-row-group">
-              <div class="table-row">
-                <div class="table-cell">대인 배상</div>
-                <div class="table-cell">무한</div>
-                <div class="table-cell">
-                  고객 자동차 보험 대인, 초과 손해 담보
-                </div>
-              </div>
-              <div class="table-row">
-                <div class="table-cell">대물 배상</div>
-                <div class="table-cell">2억원</div>
-                <div class="table-cell">고객의 자동차 이외의 대물 손해</div>
-              </div>
-              <div class="table-row">
-                <div class="table-cell">자차손해</div>
-                <div class="table-cell">8천만원</div>
-                <div class="table-cell">고객의 자동차에 대한 손해 보장</div>
-              </div>
-            </div>
+    <div class="w-3/5 flex flex-col items-start">
+      <span class="text-3xl">1. 사고 및 책임 보상</span>
+      <img class="my-20" src="../assets/policy/img-meritz.jpg" alt="" />
+      <table class="w-full table-auto">
+        <thead class="bg-primary text-white">
+          <tr>
+            <th class="font-normal py-3">항목</th>
+            <th class="font-normal py-3">보장</th>
+            <th class="font-normal py-3">비고</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="text-center">
+            <td class="py-3 bg-ywGray5 border-b border-ywGray6">대인 배상</td>
+            <td class="py-3 border-r border-b border-ywGray7">무한</td>
+            <td class="py-3 border-b border-ywGray7">
+              고객 자동차 보험 대인, 초과 손해 담보
+            </td>
+          </tr>
+          <tr class="text-center">
+            <td class="py-3 bg-ywGray5 border-b border-ywGray6">W대물 배상</td>
+            <td class="py-3 border-r border-b border-ywGray7">2억원</td>
+            <td class="py-3 border-b border-ywGray7">
+              고객의 자동차 이외의 대물 손해
+            </td>
+          </tr>
+          <tr class="text-center">
+            <td class="py-3 bg-ywGray5">자차손해</td>
+            <td class="py-3 border-r border-b border-ywGray7">8천만원e</td>
+            <td class="py-3 border-b border-ywGray7">
+              고객의 자동차에 대한 손해 보장
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div class="mt-20 mb-10 flex items-center gap-x-6">
+        <div
+          class="h-[147px] w-[280px] border border-ywGray rounded-xl flex flex-col justify-center items-center gap-y-5"
+        >
+          <img src="../assets/icon/ic-call.svg" alt="" />
+          <span class="text-base">피해접수 1800-9883</span>
+        </div>
+        <div class="w-[34px] h-px bg-ywGray"></div>
+        <div
+          class="h-[147px] w-[280px] border border-ywGray rounded-xl flex flex-col justify-center items-center gap-y-5"
+        >
+          <img src="../assets/icon/ic-center.svg" alt="" />
+          <span class="text-base">사고 및 보상처리</span>
+        </div>
+        <div class="w-[34px] h-px bg-ywGray"></div>
+        <div
+          class="h-[147px] w-[280px] border border-ywGray rounded-xl flex flex-col justify-center items-center gap-y-5"
+        >
+          <img src="../assets/icon/ic-money.svg" alt="" />
+          <span class="text-base">대리보험 및 현금 보상</span>
+        </div>
+      </div>
+
+      <div class="flex flex-col my-10 gap-y-5">
+        <span class="text-lg"
+          >- 대인배상은 고객님의 책임보험 초과 손해 시 적용</span
+        >
+        <span class="text-lg"
+          >- 기사를 제외한 모든 사람은 대인에 해당되며 무한으로 처리</span
+        >
+        <span class="text-lg"
+          >- 대리운전 보험에서 자손의 범위는 기사만 해당</span
+        >
+      </div>
+      <div class="my-16 w-full h-px bg-ywGray2"></div>
+
+      <span class="text-3xl">2. 범칙금 및 과태료 보상</span>
+      <div class="mt-10 flex flex-col gap-y-10">
+        <span class="text-lg"
+          >- 범칙금 및 과태료를 7일 이내 완벽 처리 보상합니다.</span
+        >
+        <div class="my-10 flex items-center gap-x-5">
+          <div
+            class="h-[147px] w-[257px] border border-ywGray rounded-xl flex flex-col justify-center items-center gap-y-5"
+          >
+            <img src="../assets/icon/ic-call.svg" alt="" />
+            <span class="text-base">피해접수 1800-9883</span>
+          </div>
+          <div class="w-[34px] h-px bg-ywGray"></div>
+          <div
+            class="h-[147px] w-[257px] border border-ywGray rounded-xl flex flex-col justify-center items-center gap-y-5"
+          >
+            <img src="../assets/icon/ic-fax.svg" alt="" />
+            <span class="text-base">팩스 혹은 이메일 송부</span>
+          </div>
+          <div class="w-[34px] h-px bg-ywGray"></div>
+          <div
+            class="h-[147px] w-[257px] border border-ywGray rounded-xl flex flex-col justify-center items-center gap-y-5"
+          >
+            <img src="../assets/icon/ic-confirm.svg" alt="" />
+            <span class="text-base">스타드라이버 운행사실 확인</span>
+          </div>
+          <div class="w-[34px] h-px bg-ywGray"></div>
+          <div
+            class="h-[147px] w-[257px] border border-ywGray rounded-xl flex flex-col justify-center items-center gap-y-5"
+          >
+            <img src="../assets/icon/ic-money.svg" alt="" />
+            <span class="text-base">보상</span>
           </div>
         </div>
       </div>
+
+      <div class="my-16 w-full h-px bg-ywGray2"></div>
+
+      <span class="text-3xl">3. 렌터카 서비스</span>
+      <span class="my-10 text-lg"
+        >- 사고로 차량입고 시 렌터카 서비스를 운영하고 있습니다.</span
+      >
     </div>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+
         primary: "#2DAAFF",
         ywGray: "#818181",
         ywGray1: "#808080",
         ywGray2: "#C4C4C4",
         ywGray3: "#F7F7F7",
         ywGray4: "#999999",
+        ywGray5: "#F1F5FF",
+        ywGray6: "#C5CDE2",
+        ywGray7: "#B3B3B3",
       }
     },
   },
